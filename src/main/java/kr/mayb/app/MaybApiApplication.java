be2291708bb2ac906c713b95b6857dba5878a29f
0AndWild <parkky3563@gmail.com>
2025-02-13 22:52:25 +0900

    feat: add project settings

---
+++ src/main/java/kr/mayb/app/MaybApiApplication.java
@@ -0,0 +1,13 @@
+package kr.mayb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/mayb/app/MaybApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mayb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
