2d5a090810b1496087301e56e8bac574f6b9e981
0AndWild <parkky3563@gmail.com>
2025-03-13 00:55:36 +0900

    refactor : clear

--- src/main/java/kr/mayb/data/model/AuthProvider.java
+++ src/main/java/kr/mayb/data/model/AuthProvider.java
@@ -1,10 +1,6 @@
 package kr.mayb.data.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import kr.mayb.enums.Provider;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +12,13 @@ public class AuthProvider extends BaseEntity {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private long id; // 클래스 이름 변경에 따라 필드 이름도 수정
+    private long id;
 
     @Column
     private long memberId;
 
     @Column(nullable = false)
-    private Provider provider; // 소셜 로그인 제공자 (예: Naver, Kakao)
+    private Provider provider;
 
     @Column(nullable = false, unique = true)
     private String providerId; 
