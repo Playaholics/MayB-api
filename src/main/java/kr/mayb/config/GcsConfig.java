571bbda8c63cc28cb7527d12d5108c75bff0ac61
0AndWild <parkky3563@gmail.com>
2025-04-29 21:14:33 +0900

    fix: fix image upload to gcs

--- src/main/java/kr/mayb/config/GcsConfig.java
+++ src/main/java/kr/mayb/config/GcsConfig.java
@@ -2,15 +2,18 @@ package kr.mayb.config;
 
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class GcsConfig {
 
     @Bean
     public Storage storage() {
         StorageOptions defaultInstance = StorageOptions.getDefaultInstance();
+        log.warn("-------------GCP Credential: " + defaultInstance.getCredentials().toString());
         return defaultInstance.getService();
     }
 }
