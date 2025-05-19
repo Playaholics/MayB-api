f28efe5adcc454c6cdd32bf033a17319ca2476a9
0AndWild <parkky3563@gmail.com>
2025-05-19 14:59:26 +0900

    feat: add registerQuestion API response

--- src/main/java/kr/mayb/controller/QnAController.java
+++ src/main/java/kr/mayb/controller/QnAController.java
@@ -4,10 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import kr.mayb.dto.QnADto;
 import kr.mayb.facade.QnAFacade;
 import kr.mayb.security.DenyAll;
 import kr.mayb.security.PermitAuthenticated;
+import kr.mayb.util.response.ApiResponse;
+import kr.mayb.util.response.Responses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +27,9 @@ public class QnAController {
     @Operation(summary = "상품 QnA 등록")
     @PermitAuthenticated
     @PostMapping("/questions")
-    public void registerQuestion(@RequestBody @Valid QnARequest qnARequest) {
-        qnAFacade.registerQuestion(qnARequest.productId(), qnARequest.question(), qnARequest.isSecret());
+    public ResponseEntity<ApiResponse<QnADto>> registerQuestion(@RequestBody @Valid QnARequest qnARequest) {
+        QnADto response = qnAFacade.registerQuestion(qnARequest.productId(), qnARequest.question(), qnARequest.isSecret());
+        return Responses.ok(response);
     }
 
     private record QnARequest(
